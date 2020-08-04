88584ec80d374e7c89929776b2c4d418f44795ed
Tyler Huffman <ty.huff24@gmail.com>
2020-08-04 17:46:14 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song
+  attr_accessor :artists, :genre, :name
+
+@@artist = []
+@@genre = []
+@@count = 0
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@genre.!include?(genre) ? @@genre.push(genre) : nil
+    @@artist.!include?(artist) ? @@artist.push(artist) : nil
+    
